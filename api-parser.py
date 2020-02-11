56808e4d33ae38d5541d4932cd99a431f935e54a
eriknson <eriks@mail.se>
2020-02-11 19:32:16 +0100

    WIP

--- api-parser.py
+++ api-parser.py
@@ -11,18 +11,35 @@ import json
 #for stop in result:
 #    print(stop.get('StopPointName'))
     
-response1 = requests.get('http://api.sl.se/api2/LineData.json?model=stoppoint&key=461aca8d48264b3f9a3f31d701fce8f8')
-response2 = requests.get('http://api.sl.se/api2/LineData.json?model=JourneyPatternPointOnLine&key=461aca8d48264b3f9a3f31d701fce8f8')
-locationOfStops = response1.json()
-listOfStops = response2.json()
+api_response1 = requests.get('http://api.sl.se/api2/LineData.json?model=stoppoint&key=461aca8d48264b3f9a3f31d701fce8f8')
+api_response2 = requests.get('http://api.sl.se/api2/LineData.json?model=JourneyPatternPointOnLine&key=461aca8d48264b3f9a3f31d701fce8f8')
 
-result1 = locationOfStops.get('ResponseData').get('Result')
-result2 = listOfStops.get('ResponseData').get('Result')
+locationOfStops = api_response1.json().get('ResponseData').get('Result')
+stopsAndLines = api_response2.json().get('ResponseData').get('Result')
 
-graphData = {}
+stopDict = {"1337":{"lon":1,"lat":2}}
 
-for stop in result1:
-    graphData.update({stop.get('StopPointNumber') : {'lon':stop.get('LocationEastingCoordinate'),'lat':stop.get('LocationNorthingCoordinate')}})
+for stop in locationOfStops:
+    stopDict[stop.get('StopPointNumber')] = {'lon':stop.get('LocationEastingCoordinate'),'lat':stop.get('LocationNorthingCoordinate'),'line':[]}
+    
+for stop in stopsAndLines:
+    stopID=stop.get('JourneyPatternPointNumber')
+    stopLineNumber=stop.get('LineNumber')
+    if stopID in stopDict:
+        # append stopLineNumber to line list in dictionary
+        stopDict[stopID].get('')
+        stopDict.update(stopID:)
+    if stopID not in stopDict:
+        stopDict[stopID] = {'lon':null,'lat':null,'line':[stop.get('LineNumber')]}
+    
+        stopDict.update(stopID:)
 
-for stop in result2:
-    graphData.update({stop.get('JourneyPatternPointNumber') : {'line':[stop.get('LineNumber')]}})
+    
+
+
+#for stop in stopsAndLines:
+#    if locationOfStops[stop.get('StopPointNumber')]
+#    graphData.update({stop.get('StopPointNumber') : {'lon':stop.get('LocationEastingCoordinate'),'lat':stop.get('LocationNorthingCoordinate')}})
+#
+#for stop in result2:
+#    graphData.update({stop.get('JourneyPatternPointNumber') : {'line':[stop.get('LineNumber')]}})
