7fb5c8e3f416a7ee633e48e4776f9923cd2e0c6e
eriknson <eriks@mail.se>
2020-02-12 11:30:46 +0100

    Added comma-separated static data

--- ul/ul-static-data.py
+++ ul/ul-static-data.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Mys
+API key: 270394d8f8c74e5191d4f235e8dd925e
 """
 
 # -*- coding: utf-8 -*-
 
 import requests
 import json
+
+gtfs_static_ul = requests.get('https://opendata.samtrafiken.se/ul/ul.zip?key=270394d8f8c74e5191d4f235e8dd925e')
+
+data = gtfs_static_ul.json()
+
