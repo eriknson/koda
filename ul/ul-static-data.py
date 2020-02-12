0836505e540f3c5196e56b2f8498359beff350a4
eriknson <eriks@mail.se>
2020-02-12 15:06:19 +0100

    Working parser! 🍺

--- ul/ul-static-data.py
+++ ul/ul-static-data.py
@@ -8,19 +8,30 @@ API key: 270394d8f8c74e5191d4f235e8dd925e
 
 import csv
 
-stopDict = {}
+stops = {}
+stopsOnTrip = {}
+routes = {}
 
 with open('./gtfs_static_data_ul/stops.csv') as file:
     reader = csv.DictReader(file)
-    # Create dictionary entry for each row in stops.csv
+    # Create dictionary key for each row in stops.csv with values on name, latitude and longitude
     for row in reader:
-        stopDict[row['stop_id']]={'stop_name':row['stop_name'],'stop_trip_id_list':[],'stop_lat':row['stop_lat'],'stop_lon':row['stop_lon']}
+        stops[row['stop_id']]={'stop_name':row['stop_name'],'stop_lat':row['stop_lat'],'stop_lon':row['stop_lon']}
+
 
 with open('./gtfs_static_data_ul/stop_times.csv') as file:
-    reader = csv.DictReader(file)
-    # Append each stops' matching trip id(s) from stop_times.csv
-    for row in reader:
-        stopDict[row['stop_id']].get('stop_trip_id_list').append(row['trip_id'])
-        print(stopDict[row['stop_id']].get('stop_trip_id_list'))
-        
+    reader1 = csv.DictReader(file)
+    # Loops through stop_times.csv and creates dictionary with stops per unique trip
+    for row in reader1:
+        if row['trip_id'] not in stopsOnTrip:
+            stopsOnTrip[row['trip_id']]=[stops[row['stop_id']]]
+        if row['trip_id'] in stopsOnTrip:
+            stopsOnTrip[row['trip_id']].append(stops[row['stop_id']])
 
+
+with open('./gtfs_static_data_ul/trips.csv') as file:
+    reader2 = csv.DictReader(file)
+    # Loops through trips.csv and creates dictionary with the first trip per unique route
+    for row in reader2:
+        if row['route_id'] not in routes:
+            routes[row['route_id']]=stopsOnTrip[row['trip_id']]
