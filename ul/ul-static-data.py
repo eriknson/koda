cf1cc6c5fa26bb0797227e4cbc527bf9cec1d4da
eriknson <eriks@mail.se>
2020-02-13 15:22:07 +0100

    .

--- ul/ul-static-data.py
+++ ul/ul-static-data.py
@@ -16,7 +16,8 @@ with open('./gtfs_static_data_ul/stops.csv') as file:
     reader = csv.DictReader(file)
     # Create dictionary key for each row in stops.csv with values on name, latitude and longitude
     for row in reader:
-        stops[row['stop_id']]={'stop_name':row['stop_name'],'stop_lat':row['stop_lat'],'stop_lon':row['stop_lon']}
+        stops[row['stop_id']] = {'stop_name': row['stop_name'],
+                                 'stop_lat': row['stop_lat'], 'stop_lon': row['stop_lon']}
 
 
 with open('./gtfs_static_data_ul/stop_times.csv') as file:
@@ -24,7 +25,7 @@ with open('./gtfs_static_data_ul/stop_times.csv') as file:
     # Loops through stop_times.csv and creates dictionary with stops per unique trip
     for row in reader1:
         if row['trip_id'] not in stopsOnTrip:
-            stopsOnTrip[row['trip_id']]=[stops[row['stop_id']]]
+            stopsOnTrip[row['trip_id']] = [stops[row['stop_id']]]
         if row['trip_id'] in stopsOnTrip:
             stopsOnTrip[row['trip_id']].append(stops[row['stop_id']])
 
@@ -34,4 +35,4 @@ with open('./gtfs_static_data_ul/trips.csv') as file:
     # Loops through trips.csv and creates dictionary with the first trip per unique route
     for row in reader2:
         if row['route_id'] not in routes:
-            routes[row['route_id']]=stopsOnTrip[row['trip_id']]
+            routes[row['route_id']] = stopsOnTrip[row['trip_id']]
